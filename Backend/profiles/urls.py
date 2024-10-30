fee42da0e26af4ae7fdd303c1cd34aa57cf89069
Ali Sina Sultani <alisinasultani255@gmail.com>
2024-10-30 11:54:54 +0430

    serializer and and view is added

--- Backend/profiles/urls.py
+++ Backend/profiles/urls.py
@@ -1,3 +1,8 @@
 from django.urls import path
 
-urlpatterns = [path("")]
+from .views import GetProfileAPIView, UpdateProfileAPIView
+
+urlpatterns = [
+    path("me/", GetProfileAPIView.as_view(), name="get_profile"),
+    path("me/update-profile/", UpdateProfileAPIView.as_view(), name="update_profile"),
+]
